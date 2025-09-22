da7416e6936c0abf7348b280a53c9e098e5ad9ac
Mia Sung <mia.sung@mail.utoronto.ca>
2025-09-22 12:11:03 -0400

    Completed Task A

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,14 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
+                // Task A: use line to populate the instance variables
+                String[] parts = line.split("\t");
+                if (parts.length >= 2) {
+                    String code = parts[1];
+                    String language = parts[0];
+                    languageCodeToLanguage.put(code, language);
+                    languageToLanguageCode.put(language, code);
+                }
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -56,8 +63,8 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return code;
+        // Task A: update this code to use the correct instance variable to return the appropriate value
+        return languageCodeToLanguage.get(code);
     }
 
     /**
@@ -66,8 +73,8 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        // Task A: update this code to use the correct instance variable to return the appropriate value
+        return languageToLanguageCode.get(language);
     }
 
     /**
