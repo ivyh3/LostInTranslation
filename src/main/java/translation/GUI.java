4e2bf51c156c330298063fb7068ae291b1e6dbe6
sungmia92 <mia.sung@mail.utoronto.ca>
2025-09-23 01:37:41 -0400

    Completed Task D

--- src/main/java/translation/GUI.java
+++ src/main/java/translation/GUI.java
@@ -1,66 +1,93 @@
 package translation;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.*;
 
-
-// TODO Task D: Update the GUI for the program to align with UI shown in the README example.
-//            Currently, the program only uses the CanadaTranslator and the user has
-//            to manually enter the language code they want to use for the translation.
-//            See the examples package for some code snippets that may be useful when updating
-//            the GUI.
 public class GUI {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JPanel countryPanel = new JPanel();
-            JTextField countryField = new JTextField(10);
-            countryField.setText("can");
-            countryField.setEditable(false); // we only support the "can" country code for now
-            countryPanel.add(new JLabel("Country:"));
-            countryPanel.add(countryField);
-
             JPanel languagePanel = new JPanel();
-            JTextField languageField = new JTextField(10);
             languagePanel.add(new JLabel("Language:"));
-            languagePanel.add(languageField);
 
-            JPanel buttonPanel = new JPanel();
-            JButton submit = new JButton("Submit");
-            buttonPanel.add(submit);
-
-            JLabel resultLabelText = new JLabel("Translation:");
-            buttonPanel.add(resultLabelText);
+            JPanel translationPanel = new JPanel();
+            translationPanel.add(new JLabel("Translation:"));
             JLabel resultLabel = new JLabel("\t\t\t\t\t\t\t");
-            buttonPanel.add(resultLabel);
+            translationPanel.add(resultLabel);
 
+            JPanel countryPanel = new JPanel();
 
-            // adding listener for when the user clicks the submit button
-            submit.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String language = languageField.getText();
-                    String country = countryField.getText();
+            Translator translator = new JSONTranslator();
 
-                    // for now, just using our simple translator, but
-                    // we'll need to use the real JSON version later.
-                    Translator translator = new CanadaTranslator();
+            JComboBox<String> languageComboBox = new JComboBox<>();
+            for(String languageCode : translator.getLanguageCodes()) {
+                LanguageCodeConverter converter = new LanguageCodeConverter();
+                languageComboBox.addItem(converter.fromLanguageCode(languageCode));
+            }
+            languagePanel.add(languageComboBox);
 
-                    String result = translator.translate(country, language);
-                    if (result == null) {
-                        result = "no translation found!";
+            String[] items = new String[translator.getCountryCodes().size()];
+            int i = 0;
+            for(String countryCode : translator.getCountryCodes()) {
+                CountryCodeConverter converter1 = new CountryCodeConverter();
+                items[i++] = converter1.fromCountryCode(countryCode);
+            }
+
+            JList<String> list = new JList<>(items);
+            JScrollPane scrollPane = new JScrollPane(list);
+            countryPanel.add(scrollPane, 0);
+
+            // Unified update method
+            Runnable updateTranslation = () -> {
+                String selectedLanguage = (String) languageComboBox.getSelectedItem();
+                String selectedCountry = list.getSelectedValue();
+
+                // Convert names to codes
+                String languageCode = null;
+                for (String code : translator.getLanguageCodes()) {
+                    LanguageCodeConverter converter = new LanguageCodeConverter();
+                    if (converter.fromLanguageCode(code).equals(selectedLanguage)) {
+                        languageCode = code;
+                        break;
                     }
-                    resultLabel.setText(result);
-
                 }
+                String countryCode = null;
+                for (String code : translator.getCountryCodes()) {
+                    CountryCodeConverter converter = new CountryCodeConverter();
+                    if (converter.fromCountryCode(code).equals(selectedCountry)) {
+                        countryCode = code;
+                        break;
+                    }
+                }
+                String answer = translator.translate(countryCode, languageCode);
+                resultLabel.setText(answer);
+            };
 
+            languageComboBox.addItemListener(new ItemListener() {
+                @Override
+                public void itemStateChanged(ItemEvent e) {
+                    if (e.getStateChange() == ItemEvent.SELECTED) {
+                        updateTranslation.run();
+                    }
+                }
+            });
+
+            list.addListSelectionListener(new ListSelectionListener() {
+                @Override
+                public void valueChanged(ListSelectionEvent e) {
+                    if (!e.getValueIsAdjusting()) {
+                        updateTranslation.run();
+                    }
+                }
             });
 
             JPanel mainPanel = new JPanel();
             mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-            mainPanel.add(countryPanel);
             mainPanel.add(languagePanel);
-            mainPanel.add(buttonPanel);
+            mainPanel.add(translationPanel);
+            mainPanel.add(countryPanel);
 
             JFrame frame = new JFrame("Country Name Translator");
             frame.setContentPane(mainPanel);
@@ -68,7 +95,12 @@ public class GUI {
             frame.pack();
             frame.setVisible(true);
 
-
+            // Update translation on startup if both selections are available
+            if (languageComboBox.getItemCount() > 0 && list.getModel().getSize() > 0) {
+                languageComboBox.setSelectedIndex(0);
+                list.setSelectedIndex(0);
+                updateTranslation.run();
+            }
         });
     }
 }
